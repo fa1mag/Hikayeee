8b20a2d65d7fe2d3e7913ce0f1bbe240354daa46
Fatma <fgulmez118@gmail.com>
2024-12-19 15:45:49 +0300

    Update hikaye.c

--- hikaye.c
+++ hikaye.c
@@ -3,7 +3,8 @@
 
 int main() {
 
-printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
+printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak için yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
 printf("Son çıkan efsanelerden sonra bu ormanın derinliklerinde güvende olmadıklarını biliyorlardı..");//Burak Gökmen hikaye başlangıcı
+printf("Artık üşümüş ve korkmuşlardı çaresizce geri dönüş yolunu bulabilmeyi diliyorlardı");//Fatma Gülmez
 
 }
